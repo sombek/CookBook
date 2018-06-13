0dcc2706cc2b3aa292abbc3ddba0cbb3d73106ea
Abdullah Hashim <sombek990@hotmail.com>
2018-06-13 22:30:05 +0300

    Generating the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEdtComponent } from './shopping-list/shopping-edt/shopping-edt.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEdtComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shopping-list/shopping-edt/shopping-edt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edt',
+  templateUrl: './shopping-edt.component.html',
+  styleUrls: ['./shopping-edt.component.css']
+})
+export class ShoppingEdtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
