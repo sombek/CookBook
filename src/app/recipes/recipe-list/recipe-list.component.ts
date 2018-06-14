1cafe87791aea7c9d145a95d0ca53d3fcf5ff32b
Abdullah Hashim <sombek990@hotmail.com>
2018-06-14 06:02:21 +0300

    adding recipe model and use it in recipe list with its html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] = [
+    new Recipe('Test','this is simply test','https://search.chow.com/thumbnail/1280/800/www.chowstatic.com/assets/2014/09/30669_spicy_slow_cooker_beef_chili_3000x2000.jpg')
+  ]
   constructor() { }
 
   ngOnInit() {
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,5 +1,11 @@
 export class Recipe {
     public name: string;
     public descripation: string;
-    
+    public imagePath: string;
+
+    constructor(name:string, descripation:string, imagePath:string){
+      this.name = name;
+      this.descripation = descripation;
+      this.imagePath = imagePath;
+    }
 }
