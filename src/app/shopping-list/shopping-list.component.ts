e299ea557bf73579b7309d96a84dbbde6c401f02
Abdullah Hashim <sombek990@hotmail.com>
2018-06-14 06:03:10 +0300

    adding shopping list with its model + edit shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients:Ingredients[] = [
+    new Ingredients('Tomato',10),
+    new Ingredients('Avocado',3)
+  ];
 
   constructor() { }
 
