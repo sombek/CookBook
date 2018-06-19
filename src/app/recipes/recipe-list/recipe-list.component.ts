cf8f141d99bee2edf84e8de5663f2fd45ce215e5
Abdullah Hashim <sombek990@hotmail.com>
2018-06-19 14:41:51 +0300

    run recipeList component, add onSelect recipe to shows the recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,11 +8,17 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes:Recipe[] = [
-    new Recipe('Test','this is simply test','https://search.chow.com/thumbnail/1280/800/www.chowstatic.com/assets/2014/09/30669_spicy_slow_cooker_beef_chili_3000x2000.jpg')
+    new Recipe('Test1','this is simply test','https://search.chow.com/thumbnail/1280/800/www.chowstatic.com/assets/2014/09/30669_spicy_slow_cooker_beef_chili_3000x2000.jpg'),
+    new Recipe('Test2','this is simply test','https://search.chow.com/thumbnail/1280/800/www.chowstatic.com/assets/2014/09/30669_spicy_slow_cooker_beef_chili_3000x2000.jpg'),
+    new Recipe('Test3','this is simply test','https://search.chow.com/thumbnail/1280/800/www.chowstatic.com/assets/2014/09/30669_spicy_slow_cooker_beef_chili_3000x2000.jpg')
   ]
+  @Output() recipeEvent = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
+  }
+  onSelectItem1(recipeEl2:Recipe){
+    this.recipeEvent.emit(recipeEl2)
   }
 
 }
